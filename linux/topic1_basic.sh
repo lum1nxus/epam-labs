ff8323abbc9118d6be728a7b5a2f5007fd82a64b
lum1nxus <vaclavgordienko@gmail.com>
2022-02-18 17:39:38 +0300

    feat: add topic1_basic solution

---
+++ linux/topic1_basic.sh
@@ -0,0 +1,8 @@
+mkdir exercices ; cd exercices
+touch fi1 fi2 ; mkdir dir1 dir2 ; touch dir1/dir2
+cp -R dir1 dir2
+cp etc/services ~/exercices
+grep HTTP services
+wc -l services
+more -p services
+ncal -0 2051
